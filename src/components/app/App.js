227c0d8f39eac3e61d5b0799e36319a5f33611c0
GothMike <117478074+GothMike@users.noreply.github.com>
2023-10-21 20:05:27 +0400

    Разработка главной страницы
    - Удалены ненужные файлы
    - Добавлена библиотеку для работы с загрузками файла, чтобы пользователи могли загружать резюме
    - Создан компонент LinkResume
    - Сверстана главная страница и оптимизирована под всех устройства

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/mainPage/MainPage";
+import LinkResume from "../linkResume/LinkResume";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter, faFontAwesome } from "@fortawesome/free-brands-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
 const App = () => {
-  return <MainPage />;
+  library.add(fas, faTwitter, faFontAwesome, fab);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      <LinkResume />
+    </Router>
+  );
 };
 
 export default App;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,34 @@
+import { Link } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <nav className="sidebar">
       <ul className="sidebar__list">
-        <li className="sidebar__item">Обо мне</li>
-        <li className="sidebar__item">Резюме</li>
-        <li className="sidebar__item">Навыки</li>
-        <li className="sidebar__item">Портфолио</li>
-        <li className="sidebar__item">Контакты</li>
+        <li className="sidebar__item">
+          <Link className="sidebar__link" to="/">
+            Главная
+          </Link>
+        </li>
+        <li className="sidebar__item">
+          <Link className="sidebar__link" to="/">
+            Обо мне
+          </Link>
+        </li>
+        <li className="sidebar__item">
+          <Link className="sidebar__link" to="/">
+            Навыки
+          </Link>
+        </li>
+        <li className="sidebar__item">
+          <Link className="sidebar__link" to="/">
+            Портфолио
+          </Link>
+        </li>
+        <li className="sidebar__item">
+          <Link className="sidebar__link  " to="/">
+            Контакты
+          </Link>
+        </li>
       </ul>
     </nav>
   );
