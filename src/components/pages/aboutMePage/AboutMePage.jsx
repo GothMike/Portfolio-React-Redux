84885675a8804189c7da2ce95b32248840078a07
GothMike <117478074+GothMike@users.noreply.github.com>
2023-10-25 00:37:05 +0400

    Разработка страницы AboutMe
    - Разработана логика по появлению текста, в зависимости от клика юзера
    - Добавлена UI/UX элементы на главную страницу
    - Добавлены анимации к иконкам

--- src/components/pages/aboutMePage/AboutMePage.jsx
+++ src/components/pages/aboutMePage/AboutMePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "../../sidebar/Sidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { toogleVisabilitySidebar } from "../../../redux/actions/sidebar";
@@ -8,30 +8,63 @@ const AboutMePage = () => {
   const dispatch = useDispatch();
   const fullscreen = useSelector((state) => state.sidebar.bigSidebar);
   const fullScreenContent = fullscreen ? "content__full" : "";
+  const [selectedCard, setSelectedCard] = useState("fullStack");
 
   useEffect(() => {
     dispatch(toogleVisabilitySidebar(true));
   }, []);
 
+  const ChooseCard = () => {
+    switch (selectedCard) {
+      case "fullStack":
+        return (
+          <p className="about__view-descr">
+            Я являюсь Full-stack разработчиком и мои обязанности охватывают как клиентскую, так и
+            серверную стороны веб-приложений. Моя работа включает создание и обслуживание API для
+            взаимодействия с клиентской частью приложения. Я также работаю с базами данных, включая
+            их создание, обновление и написание запросов. На клиентской стороне я занимаюсь
+            разработкой интерактивных и динамических пользовательских интерфейсов, придавая
+            приложениям высокую производительность и обеспечивая управление состоянием приложения.
+          </p>
+        );
+      case "frontend":
+        return (
+          <p className="about__view-descr">
+            Я разрабатываю веб-приложения и веб-сайты, фокусируясь на клиентской стороне. Моя работа
+            включает в себя создание интерактивных и динамических пользовательских интерфейсов,
+            обеспечение высокой производительности приложений, а также управление состоянием
+            приложения.
+          </p>
+        );
+      case "backend":
+        return (
+          <p className="about__view-descr">
+            Я разработываю серверную часть веб-приложения. Моя работа включает в себя создание и
+            обслуживание API для взаимодействия с клиентской частью приложения. Я также занимаетесь
+            работой с базами данных, включая создание, обновление и запросы к данным.
+          </p>
+        );
+      default:
+        return <p className="about__view-descr">Я разрабатываю full-stack приложения</p>;
+    }
+  };
+
+  const cardStyles = {
+    fullStack: selectedCard === "fullStack" ? "about__card_selected" : "",
+    frontend: selectedCard === "frontend" ? "about__card_selected" : "",
+    backend: selectedCard === "backend" ? "about__card_selected" : "",
+  };
+
   return (
     <div className="page">
-      <Sidebar hidden={true} />
+      <Sidebar />
       <main className={`content ${fullScreenContent}`}>
         <section className="about">
           <div className="about__cards">
-            <div className="about__card">
-              <div className="about__card-header">
-                <div className="about__card-title">Frontend разработка</div>
-                <div className="about__card-icon">
-                  <FontAwesomeIcon icon="fa-solid fa-code" style={{ color: "#559fff" }} size="xl" />
-                </div>
-              </div>
-              <p className="about__card-descr">
-                Я создаю интерфейсы для веб-приложений, обеспечивая их функциональность, структуру и
-                дизайн, чтобы пользователи могли взаимодействовать с веб-сайтами и приложениями.
-              </p>
-            </div>
-            <div className="about__card">
+            <div
+              onClick={() => setSelectedCard("backend")}
+              className={`about__card ${cardStyles.backend}`}
+            >
               <div className="about__card-header">
                 <div className="about__card-title">Backend разработка</div>
                 <div className="about__card-icon">
@@ -47,9 +80,27 @@ const AboutMePage = () => {
                 надежность, производительность и безопасность.
               </p>
             </div>
-            <div className="about__card">
+            <div
+              onClick={() => setSelectedCard("frontend")}
+              className={`about__card ${cardStyles.frontend}`}
+            >
               <div className="about__card-header">
-                <div className="about__card-title">Full-stack разработка</div>
+                <div className="about__card-title">Frontend разработка</div>
+                <div className="about__card-icon">
+                  <FontAwesomeIcon icon="fa-solid fa-code" style={{ color: "#559fff" }} size="xl" />
+                </div>
+              </div>
+              <p className="about__card-descr">
+                Я создаю интерфейсы для веб-приложений, обеспечивая их функциональность, структуру и
+                дизайн, чтобы пользователи могли взаимодействовать с веб-сайтами и приложениями.
+              </p>
+            </div>
+            <div
+              onClick={() => setSelectedCard("fullStack")}
+              className={`about__card ${cardStyles.fullStack}`}
+            >
+              <div className="about__card-header">
+                <div className="about__card-title">Full-Stack разработка</div>
                 <div className="about__card-icon">
                   <FontAwesomeIcon
                     icon="fa-solid fa-laptop-code"
@@ -68,16 +119,9 @@ const AboutMePage = () => {
           <div className="about__view">
             <div className="about__view-title">Привет, я Михаил Воронин </div>
             <div className="about__view-subtitle">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis repellendus vero
+              Моя работа заключается в создании современных веб-приложений
             </div>
-            <p className="about__view-descr">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet assumenda totam
-              exercitationem id commodi debitis ipsum nam facilis aperiam, quasi iste suscipit iusto
-              consequuntur cumque blanditiis neque non. Ex, tempore. Architecto molestiae
-              voluptatibus nemo distinctio ullam rerum odio debitis amet eum corrupti perferendis
-              magni pariatur provident placeat fuga modi eius, atque quaerat quo fugiat a culpa
-              nihil! Illum, ut suscipit.
-            </p>
+            <ChooseCard />
           </div>
         </section>
       </main>
--- src/components/pages/mainPage/MainPage.jsx
+++ src/components/pages/mainPage/MainPage.jsx
@@ -51,6 +51,17 @@ const MainPage = () => {
               </Button>
             </div>
           </div>
+          <div className="content__stickers">
+            <img className="content__sticker" src="./icons/icon-html.svg" alt="icon-html" />
+            <img className="content__sticker" src="./icons/icon-css.svg" alt="icon-css" />
+            <img className="content__sticker" src="./icons/icon-git.svg" alt="icon-git" />
+            <img className="content__sticker" src="./icons/icon-js.webp" alt="icon-js" />
+            <img className="content__sticker" src="./icons/icon-react.svg" alt="icon-react" />
+            <img className="content__sticker" src="./icons/icon-redux.svg" alt="icon-redux" />
+            <img className="content__sticker" src="./icons/icon-sharp.svg" alt="icon-sharp" />
+            <img className="content__sticker" src="./icons/icon-api.webp" alt="icon-api" />
+            <img className="content__sticker" src="./icons/icon-sql.webp" alt="icon-sql" />
+          </div>
         </main>
       </div>
     </>
